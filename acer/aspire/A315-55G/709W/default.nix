9c4b013f382caac90309e58023568871fce30e26
Anubhav Kini <anubhavkini@pm.me>
2021-10-24 11:00:37 +0530

    Enable Nvidia GPU

--- acer/aspire/A315-55G/709W/default.nix
+++ acer/aspire/A315-55G/709W/default.nix
@@ -3,8 +3,8 @@
 {
   imports = [
     ../../../../common/cpu/intel
-#    ../../../../common/gpu/nvidia.nix
-    ../../../../common/gpu/nvidia-disable.nix
+    ../../../../common/gpu/nvidia.nix
+#   ../../../../common/gpu/nvidia-disable.nix
     ../../../../common/pc/laptop
     ../../../../common/pc/laptop/hdd
   ];
@@ -22,29 +22,28 @@
   
   boot.kernelModules = lib.mkDefault [ "kvm-intel" ];
 
-#  hardware.nvidia.prime = {
-#    intelBusId = lib.mkDefault "PCI:0:2:0";
-#    nvidiaBusId = lib.mkDefault "PCI:2:0:0";
-#  };
+  hardware.nvidia.prime = {
+    intelBusId = lib.mkDefault "PCI:0:2:0";
+    nvidiaBusId = lib.mkDefault "PCI:2:0:0";
+  };
 
-#  hardware.nvidia.powerManagement.enable = lib.mkDefault true;
-#  hardware.nvidia.powerManagement.finegrained = lib.mkDefault true;
+  hardware.nvidia.powerManagement.enable = lib.mkDefault true;
+  hardware.nvidia.powerManagement.finegrained = lib.mkDefault true;
 
-  # Nvidia's systemd power management doesn't support
-  # suspend-then-hibernate
-#  systemd.services.nvidia-suspend-then-hibernate = {
-#    description = "NVIDIA system suspend-then-hibernate actions";
-#  	before = [ "systemd-suspend-then-hibernate.service" ];
-#  	requiredBy = [ "systemd-suspend-then-hibernate.service" ];
-#  	path = with pkgs; [ kbd ];
-#  	serviceConfig = {
-#  	  Type = "oneshot";
-#  	  ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh suspend";
-#  	};
-#  };
+  # Nvidia's systemd power management doesn't support suspend-then-hibernate
+  systemd.services.nvidia-suspend-then-hibernate = {
+    description = "NVIDIA system suspend-then-hibernate actions";
+  	before = [ "systemd-suspend-then-hibernate.service" ];
+  	requiredBy = [ "systemd-suspend-then-hibernate.service" ];
+  	path = with pkgs; [ kbd ];
+  	serviceConfig = {
+  	  Type = "oneshot";
+  	  ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh suspend";
+  	};
+  };
 
-#  systemd.services.nvidia-resume = {
-#    after = [ "systemd-suspend-then-hibernate.service" ];
-#    requiredBy = [ "systemd-suspend-then-hibernate.service" ];
-#  };
+  systemd.services.nvidia-resume = {
+    after = [ "systemd-suspend-then-hibernate.service" ];
+    requiredBy = [ "systemd-suspend-then-hibernate.service" ];
+  };
 }
