2e39ac5ec45b883e601b8a8abc6a4f065dd7d7bf
Anubhav Kini <anubhavkini@pm.me>
2022-01-05 09:38:10 +0530

    Disable Nvidia GPU

--- acer/aspire/a315-55g/default.nix
+++ acer/aspire/a315-55g/default.nix
@@ -6,7 +6,7 @@ in
 {
   imports = [
     ../../../common/cpu/intel
-    ../../../common/gpu/nvidia.nix
+    ../../../common/gpu/nvidia-disable.nix
     ../../../common/pc/laptop
     ../../../common/pc/laptop/hdd
   ];
@@ -30,33 +30,4 @@ in
 #  hardware.opengl.extraPackages = with pkgs; [
 #    intel-compute-runtime
 #  ];
-
-  hardware.nvidia = {
-    prime = {
-      intelBusId = lib.mkDefault "PCI:0:2:0";
-      nvidiaBusId = lib.mkDefault "PCI:2:0:0";
-    };
-
-    powerManagement = {
-      enable = lib.mkDefault true;
-      finegrained = lib.mkDefault true;
-    };
-  };
-
-  # Nvidia's systemd power management doesn't support suspend-then-hibernate
-  systemd.services.nvidia-suspend-then-hibernate = lib.mkIf cfg.enable {
-    description = "NVIDIA system suspend-then-hibernate actions";
-    before = [ "systemd-suspend-then-hibernate.service" ];
-    requiredBy = [ "systemd-suspend-then-hibernate.service" ];
-    path = with pkgs; [ kbd ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${config.hardware.nvidia.package.out}/bin/nvidia-sleep.sh suspend";
-    };
-  };
-
-  systemd.services.nvidia-resume = lib.mkIf cfg.enable {
-    after = [ "systemd-suspend-then-hibernate.service" ];
-    requiredBy = [ "systemd-suspend-then-hibernate.service" ];
-  };
 }
